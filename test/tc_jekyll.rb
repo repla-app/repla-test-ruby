e7471752f32ae1edc024ad6e77af4ca4b6cd6e06
Roben Kleene <contact@robenkleene.com>
2020-05-08 11:58:50 -0400

    Add second test

--- test/tc_jekyll.rb
+++ test/tc_jekyll.rb
@@ -8,10 +8,14 @@ class TestJekyll < Minitest::Test
   JEKYLL_DIRECTORY = File.expand_path(
     File.join(__dir__, '../external/repla-test-jekyll/')
   )
-  COMMAND = 'repla jekyll'.freeze
+  JEKYLL_DIRECTORY_ARCHITECT = File.expand_path(
+    File.join(__dir__, '../external/repla-test-jekyll-architect/')
+  )
   TEST_TITLE_PREFIX = 'Your awesome title'.freeze
+  TEST_TITLE_PREFIX_ARCHITECT = 'Architect theme'.freeze
+  COMMAND = 'repla jekyll'.freeze
 
-  def setup
+  def test_jekyll
     Dir.chdir(JEKYLL_DIRECTORY) do
       `#{COMMAND}`
     end
@@ -21,20 +25,35 @@ class TestJekyll < Minitest::Test
       !window_id.nil?
     end
     refute_nil(window_id)
-    @window = Repla::Window.new(window_id)
-  end
-
-  def teardown
-    @window.close
-  end
-
-  def test_jekyll
+    window = Repla::Window.new(window_id)
     javascript = File.read(Repla::Test::TITLE_JAVASCRIPT_FILE)
     result = nil
     Repla::Test.block_until do
-      result = @window.do_javascript(javascript)
+      result = window.do_javascript(javascript)
       result && result.start_with?(TEST_TITLE_PREFIX)
     end
     assert(result.start_with?(TEST_TITLE_PREFIX))
+    window.close
+  end
+
+  def test_jekyll_architect
+    Dir.chdir(JEKYLL_DIRECTORY_ARCHITECT) do
+      `#{COMMAND}`
+    end
+    window_id = nil
+    Repla::Test.block_until do
+      window_id = Repla::Test::Helper.window_id
+      !window_id.nil?
+    end
+    refute_nil(window_id)
+    window = Repla::Window.new(window_id)
+    javascript = File.read(Repla::Test::TITLE_JAVASCRIPT_FILE)
+    result = nil
+    Repla::Test.block_until do
+      result = window.do_javascript(javascript)
+      result && result.start_with?(TEST_TITLE_PREFIX_ARCHITECT)
+    end
+    assert(result.start_with?(TEST_TITLE_PREFIX_ARCHITECT))
+    window.close
   end
 end
